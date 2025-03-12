cee56cb714031af0e8d24baa914bfcfa42bc9f4b
MinecraftPEayer <87757022+MinecraftPEayer@users.noreply.github.com>
2025-03-13 07:13:15 +0800

    hotfix: lmao/uwu.php

--- src/app/lmao/uwu.php/page.tsx
+++ src/app/lmao/uwu.php/page.tsx
@@ -11,10 +11,12 @@ export const viewport: Viewport = {
     themeColor: 0xFF0000
 }
 
-export default const Lmao = () => {
+const Lmao = () => {
     return (<div>
         <p>這網頁不是詐騙，但是如果你想被騙就點底下的按鈕</p>
         <button onClick="location.href = 'https://youtu.be/dQw4w9WgXcQ'">如果你想被騙就點我</button>
     </div>)
 }
+
+export default Lmao;
     
